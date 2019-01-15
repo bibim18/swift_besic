fb057038275952fe3cc616589c7a362b98266613
Admin <admin@MAC3.local>
2019-01-15 11:46:27 +0700

    mySecondApp

---
+++ mySecondApp/mySecondApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  mySecondApp
+//
+//  Created by Admin on 15/1/2562 BE.
+//  Copyright © 2562 Saiwarun.Y. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        showResult.text = ""
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
