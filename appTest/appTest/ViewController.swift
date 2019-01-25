5f4ddc29029712325218d6ab13d017008b7a66c9
Admin <admin@MAC3.local>
2019-01-25 14:31:20 +0700

    display results ML

--- appTest/appTest/ViewController.swift
+++ appTest/appTest/ViewController.swift
@@ -8,8 +8,9 @@
 
 import UIKit
 import AVKit
+import Vision
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, AVCaptureVideoDataOutputSampleBufferDelegate {
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -29,8 +30,27 @@ class ViewController: UIViewController {
         let previewLayer = AVCaptureVideoPreviewLayer(session: captureSession)
         view.layer.addSublayer(previewLayer)
         previewLayer.frame = view.frame
+        
+        let dataOutput = AVCaptureVideoDataOutput()
+        dataOutput.setSampleBufferDelegate(self, queue: DispatchQueue(label: "videoQueue"))
+        captureSession.addOutput(dataOutput)
+
+        //            VNImageRequestHandler(cgImage: , options: [:]).perform(request:)
     }
 
-
+    func captureOutput(_ output: AVCaptureOutput, didOutput
+        sampleBufer: CMSampleBuffer, from connection: AVCaptureConnection) {
+//        print("Camera was able to capture a frame: ", Date())
+        
+        guard let pixekBuffer: CVPixelBuffer = CMSampleBufferGetImageBuffer(sampleBufer) else { return }
+        guard let model = try? VNCoreMLModel(for: Resnet50().model) else { return }
+        let request = VNCoreMLRequest(model: model){
+            (finishedReq, err) in
+//            print(finishedReq.results)
+            guard let results = finishedReq.results as? [VNClassificationObservation] else { return }
+            guard let firstObservation = results.first else { return }
+            print(firstObservation.identifier, firstObservation.confidence)
+        }
+        try? VNImageRequestHandler(cvPixelBuffer: pixekBuffer, options: [:]).perform([request])    }
 }
 
